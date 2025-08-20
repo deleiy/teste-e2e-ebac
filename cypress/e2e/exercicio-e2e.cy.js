dad0c20cab1a094bb788b767ff7259deab517084
vanderlei <derlei1.vanderlei@gmail.com>
2025-08-19 22:45:35 -0300

    alteracao no exercicio-e2e-cy.js e comands.js

--- cypress/e2e/exercicio-e2e.cy.js
+++ cypress/e2e/exercicio-e2e.cy.js
@@ -1,16 +1,16 @@
-// <reference types="cypress" />
+//  cypress/e2e/exercicio-e2e.cy.js
 
 context('Exercicio - Testes End-to-end - Fluxo de pedido', () => {
-    /* Como cliente 
-        Quero acessar a Loja EBAC 
-        Para fazer um pedido de 4 produtos 
+    /* Como cliente
+        Quero acessar a Loja EBAC
+        Para fazer um pedido de 4 produtos
         Fazendo a escolha dos produtos
         Adicionando ao carrinho
         Preenchendo todas opções no checkout
         E validando minha compra ao final */
 
     beforeEach(() => {
-        cy.visit('http://lojaebac.ebaconline.art.br/produtos/'); 
+        cy.visit('http://lojaebac.ebaconline.art.br/produtos/');
     });
 
     it('Deve fazer um pedido na loja Ebac Shop de ponta a ponta', () => {
@@ -24,10 +24,10 @@ context('Exercicio - Testes End-to-end - Fluxo de pedido', () => {
                 cy.wrap($el).click();
             });
             
-            cy.wait(500); 
+            cy.get( {timeout: 10000});
 
             cy.get('.single_add_to_cart_button').click();
-
+             
             
             cy.visit('http://lojaebac.ebaconline.art.br/produtos/');
         });
@@ -35,16 +35,20 @@ context('Exercicio - Testes End-to-end - Fluxo de pedido', () => {
         cy.visit('http://lojaebac.ebaconline.art.br/carrinho/'); 
         cy.get('.checkout-button').click();
         
-        cy.get('#billing_first_name').type('Seu_Nome');
-        cy.get('#billing_last_name').type('Seu_Sobrenome');
-        cy.get('#billing_company').type('Sua_Empresa');
-        cy.get('#billing_address_1').type('Rua da Lapa, 123');
-        cy.get('#billing_city').type('São Paulo');
-        cy.get('#billing_postcode').type('01234-567');
-        cy.get('#billing_phone').type('11987654321');
-        cy.get('#billing_email').type('teste@teste.com');
-        cy.get('#terms').check();
+        const customerData = {
+            nome: 'Seu_Nome',
+            sobrenome: 'Seu_Sobrenome',
+            empresa: 'Sua_Empresa',
+            endereco: 'Rua da Lapa, 123',
+            cidade: 'São Paulo',
+            cep: '01234-567',
+            telefone: '11987654321',
+            email: 'teste@teste.com'
+        };
+
+        cy.preencherDadosCheckout(customerData);
+        
         cy.get('#place_order').click();
         cy.get('.woocommerce-notice').should('contain', 'Obrigado. Seu pedido foi recebido.');
-    });
+    }); 
 });
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,6 +1,11 @@
-Cypress.Commands.add('login', (usuario, senha) => {
-    cy.get('#username').type(usuario)
-    cy.get('#password').type(senha, {log: false})
-    cy.get('.woocommerce-form > .button').click()
+Cypress.Commands.add('preencherDadosCheckout', (dados) => {
+    cy.get('#billing_first_name').type(dados.nome);
+    cy.get('#billing_last_name').type(dados.sobrenome);
+    cy.get('#billing_company').type(dados.empresa);
+    cy.get('#billing_address_1').type(dados.endereco);
+    cy.get('#billing_city').type(dados.cidade);
+    cy.get('#billing_postcode').type(dados.cep);
+    cy.get('#billing_phone').type(dados.telefone);
+    cy.get('#billing_email').type(dados.email);
+    cy.get('#terms').check();
 });
-
